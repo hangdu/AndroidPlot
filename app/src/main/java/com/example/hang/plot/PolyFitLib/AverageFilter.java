2bcd5e6230429cbbe323681b1977626807f95709
hangdu <2580392278@qq.com>
2018-04-03 16:56:39 -0400

    first commit

---
+++ app/src/main/java/com/example/hang/plot/PolyFitLib/AverageFilter.java
@@ -0,0 +1,32 @@
+package com.example.hang.plot.PolyFitLib;
+
+/**
+ * Created by hang on 4/2/18.
+ */
+
+
+public class AverageFilter {
+    int windowSize;
+    double[] yArray;
+    public AverageFilter(int size, double[] array) {
+        windowSize = size;
+        yArray = array;
+    }
+
+    public double[] filter() {
+        double[] res = new double[yArray.length];
+        for (int i = windowSize-1; i < res.length; i++) {
+            //find res[i]
+            double temp = 0;
+            for (int j = i+1-windowSize; j <= i; j++) {
+                temp += yArray[j];
+            }
+            res[i] = temp/windowSize;
+        }
+
+        for (int i = 0; i <  windowSize; i++) {
+            res[i] = yArray[i];
+        }
+        return res;
+    }
+}
